d34628a935cdd94cdd60caad53f9e796d999ae07
Max Coplan <mchcopl@gmail.com>
2019-02-21 19:11:08 -0500

    Improve GAN and SQL objects

--- ML/OOD/gan.py
+++ ML/OOD/gan.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 # Matt
 
+import numpy as np
+import pandas as pd
+
 from keras.layers import Input
 from keras.models import Model
 from keras.optimizers import Adam
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, classification_report
 
 from discriminator import Discriminator
 from generator import Generator
 
-import numpy as np
-import pandas as pd
 
 try:
     import cPickle as pickle
 except:
     import pickle
-from sklearn.metrics import confusion_matrix, classification_report
+
 
 
 class GAN(object):
-    
+
     #######################################################
     # Attributes.
     #######################################################
@@ -52,95 +54,97 @@ class GAN(object):
         batch_size = 256
         epochs = 7000
         optimizer = Adam(0.0002, 0.5)
-        
-        dataframe = pd.read_csv('../../../CSV/kdd_neptune_only_5000.csv').sample(500) # sample 100 data points randomly from the csv
-        
+        # sample 100 data points randomly from the csv
+        dataframe = pd.read_csv('../../../CSV/kdd_neptune_only_5000.csv').sample(500)
+
         # apply "le.fit_transform" to every column (usually only works on 1 column)
         le = LabelEncoder()
         dataframe_encoded = dataframe.apply(le.fit_transform)
         dataset = dataframe_encoded.values
-        
+
         #to visually judge results
         print("Real neptune attacks:")
         print(dataset[:2])
-        
+
         # Set X as our input data and Y as our label
         X_train = dataset[:, 0:41].astype(float)
         Y_train = dataset[:, 41]
-        
+
         # labels for data. 1 for valid attacks, 0 for fake (generated) attacks
         valid = np.ones((batch_size, 1))
         fake = np.zeros((batch_size, 1))
-        
+
         # build the discriminator portion
         self.discriminator = Discriminator().get()
         self.discriminator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-        
+
         # build the generator portion
         self.generator = Generator(self.attack_type).get()
-        
+
         #input and output of our combined model
         z = Input(shape=(41,))
         attack = self.generator(z)
         validity = self.discriminator(attack)
-        
+
         # build combined model from generator and discriminator
         self.gan = Model(z, validity)
         self.gan.compile(loss='binary_crossentropy', optimizer=optimizer)
-        
+
         #break condition for training (when diverging)
-        loss_increase_count = 0;
-        prev_g_loss = 0;
-        
+        loss_increase_count = 0
+        prev_g_loss = 0
+
         for epoch in range(epochs):
 
             # ---------------------
             #  Train Discriminator
             # ---------------------
-            
+
             # selecting batch_size random attacks from our training data
             idx = np.random.randint(0, X_train.shape[0], batch_size)
             attacks = X_train[idx]
-            
+
             # generate a matrix of noise vectors
             noise = np.random.normal(0, 1, (batch_size, 41))
-            
+
             # create an array of generated attacks
             gen_attacks = self.generator.predict(noise)
-            
+
             # loss functions, based on what metrics we specify at model compile time
             d_loss_real = self.discriminator.train_on_batch(attacks, valid)
             d_loss_fake = self.discriminator.train_on_batch(gen_attacks, fake)
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-            
+
             # generator loss function
             g_loss = self.gan.train_on_batch(noise, valid)
-            
+
             if epoch % 100 == 0:
-                print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f] [Loss change: %.3f, Loss increases: %.0f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss, g_loss - prev_g_loss, loss_increase_count))
-            
+                print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f] [Loss change:\
+                      %.3f, Loss increases: %.0f]"\
+                      % (epoch, d_loss[0], 100 * d_loss[1], g_loss, g_loss - prev_g_loss, loss_increase_count))
+
             # if our generator loss icreased this iteration, increment the counter by 1
             if (g_loss - prev_g_loss) > 0:
                 loss_increase_count = loss_increase_count + 1
-            else: 
+            else:
                 loss_increase_count = 0  # otherwise, reset it to 0, we are still training effectively
-                
+
             prev_g_loss = g_loss
-                
+
             if loss_increase_count > 5:
                 print('Stoping on iteration: ', epoch)
                 break
-                
+
             if epoch % 20 == 0:
                 f = open("../../../Results/GANresultsNeptune.txt", "a")
                 np.savetxt("../../../Results/GANresultsNeptune.txt", gen_attacks, fmt="%.0f")
                 f.close()
-                
+
         # peek at our results
         results = np.loadtxt("../../../Results/GANresultsNeptune.txt")
         print("Generated Neptune attacks: ")
         print(results[:2])
-    
+
     #############################################################################
     # I'd suggest that we have a test method native to the object.
     # I.e. a GAN should know how to test itself, at least basically
@@ -175,8 +179,8 @@ class GAN(object):
         self.save_file = filename + ".pickle"
         with open(self.save_file, 'wb') as f:
             pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
-    
-    def load_state_from_file(self, filename = None):
+
+    def load_state_from_file(self, filename=None):
         if not filename:
             filename = self.save_file
             if not filename:
@@ -187,14 +191,14 @@ class GAN(object):
             tmp_dict = pickle.load(f)
             self.__dict__.update(tmp_dict.__dict__)
             f.close()
-        
+
 
 
 def main():
     """ Auto run main method """
     attack_type = "neptune"
     gan = GAN(attack_type)
-    
+
     gan.train()
     # print(gan.discriminator())
 
--- ML/OOD/mysql.py
+++ ML/OOD/mysql.py
@@ -9,18 +9,18 @@ class SQLConnector(object):
         """ Constructor """
         # Connect to the database
         self.connection = pymysql.connect(host='localhost',
-                             port=3306,
-                             user='ldeng',
-                             password='cs*titanML',
-                             db='results',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+                                          port=3306,
+                                          user='ldeng',
+                                          password='cs*titanML',
+                                          db='results',
+                                          charset='utf8mb4',
+                                          cursorclass=pymysql.cursors.DictCursor)
 
-    def create_hyper(self):
-        """ Creates the hypers table """
+        def create_hyper(self):
+            """ Creates the hypers table """
         try:
             with self.connection.cursor() as cursor:
-                sql = "CREATE TABLE hypers (id int, iteration int, layers varchar(255), attack varchar(255), accuracy float(20));"                
+                sql = "CREATE TABLE hypers (id int, iteration int, layers varchar(255), attack varchar(255), accuracy float(20));"
                 cursor.execute(sql)
             self.connection.commit()
         finally:
@@ -36,18 +36,18 @@ class SQLConnector(object):
                 cursor.execute(sql1, (str(theid)))
                 dicti = cursor.fetchall()[0]
                 maxnum = dicti.get('MAX(iteration)')
-                if maxnum == None:
+                if maxnum is None:
                     maxnum = 0
                 maxnum += 1
                 cursor.execute(sql2, (str(theid), str(maxnum), str(layersstr), str(attack), str(acc)))
-                
+
             # connection is not autocommit by default. So you must commit to save
             # your changes.
             self.connection.commit()
 
         finally:
             pass
- 
+
     def sort_hyper(self):
         """ Reorders the hypers table based off of id """
         try:
@@ -58,7 +58,7 @@ class SQLConnector(object):
         finally:
             pass
 
-    def read_hyper(self, acc = 0):
+    def read_hyper(self, acc=0):
         """ Reads from the hypers table dependent on accuracy """
         try:
             with self.connection.cursor() as cursor:
@@ -81,14 +81,38 @@ class SQLConnector(object):
         finally:
             pass
 
-    def write_gens(self, theid, modelnum, iteration, duration, protocol_type, service, flag, src_bytes, dst_bytes, land, wrong_fragment, urgent, hot, num_failed_logins, logged_in, num_compromised, root_shell, su_attempted, num_root, num_file_creations, num_shells, num_access_files, num_outbound_cmds, is_host_login, is_guest_login, count, srv_count, serror_rate, srv_serror_rate, rerror_rate, srv_rerror_rate, same_srv_rate, diff_srv_rate, srv_diff_host_rate, dst_host_count, dst_host_srv_count, dst_host_same_srv_rate, dst_host_diff_srv_rate, dst_host_same_src_port_rate, dst_host_srv_diff_host_rate, dst_host_serror_rate, dst_host_srv_serror_rate, dst_host_rerror_rate, dst_host_srv_rerror_rate, attack_type):
+    def write_gens(self, theid, modelnum, iteration, duration, protocol_type, service, flag,
+                   src_bytes, dst_bytes, land, wrong_fragment, urgent, hot, num_failed_logins, logged_in,
+                   num_compromised, root_shell, su_attempted, num_root, num_file_creations, num_shells,
+                   num_access_files, num_outbound_cmds, is_host_login, is_guest_login, count, srv_count,
+                   serror_rate, srv_serror_rate, rerror_rate, srv_rerror_rate, same_srv_rate,
+                   diff_srv_rate, srv_diff_host_rate, dst_host_count, dst_host_srv_count,
+                   dst_host_same_srv_rate, dst_host_diff_srv_rate, dst_host_same_src_port_rate,
+                   dst_host_srv_diff_host_rate, dst_host_serror_rate, dst_host_srv_serror_rate,
+                   dst_host_rerror_rate, dst_host_srv_rerror_rate, attack_type):
         """ Writes to the gens table """
         try:
             with self.connection.cursor() as cursor:
                 sql = "insert into gens (id, modelnum, iteration, duration, protocol_type, service, flag, src_bytes, dst_bytes, land, wrong_fragment, urgent, hot, num_failed_logins, logged_in, num_compromised, root_shell, su_attempted, num_root, num_file_creations, num_shells, num_access_files, num_outbound_cmds, is_host_login, is_guest_login, count, srv_count, serror_rate, srv_serror_rate, rerror_rate, srv_rerror_rate, same_srv_rate, diff_srv_rate, srv_diff_host_rate, dst_host_count, dst_host_srv_count, dst_host_same_srv_rate, dst_host_diff_srv_rate, dst_host_same_src_port_rate, dst_host_srv_diff_host_rate, dst_host_serror_rate, dst_host_srv_serror_rate, dst_host_rerror_rate, dst_host_srv_rerror_rate, attack_type) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
 
-                cursor.execute(sql, (str(theid), str(modelnum), str(iteration), str(duration), str(protocol_type), str(service), str(flag), str(src_bytes), str(dst_bytes), str(land), str(wrong_fragment), str(urgent), str(hot), str(num_failed_logins), str(logged_in), str(num_compromised), str(root_shell), str(su_attempted), str(num_root), str(num_file_creations), str(num_shells), str(num_access_files), str(num_outbound_cmds), str(is_host_login), str(is_guest_login), str(count), str(srv_count), str(serror_rate), str(srv_serror_rate), str(rerror_rate), str(srv_rerror_rate), str(same_srv_rate), str(diff_srv_rate), str(srv_diff_host_rate), str(dst_host_count), str(dst_host_srv_count), str(dst_host_same_srv_rate), str(dst_host_diff_srv_rate), str(dst_host_same_src_port_rate), str(dst_host_srv_diff_host_rate), str(dst_host_serror_rate), str(dst_host_srv_serror_rate), str(dst_host_rerror_rate), str(dst_host_srv_rerror_rate), str(attack_type)))
-                
+                cursor.execute(sql, (str(theid), str(modelnum), str(iteration), str(duration),
+                                     str(protocol_type), str(service), str(flag), str(src_bytes),
+                                     str(dst_bytes), str(land), str(wrong_fragment), str(urgent),
+                                     str(hot), str(num_failed_logins), str(logged_in),
+                                     str(num_compromised), str(root_shell), str(su_attempted),
+                                     str(num_root), str(num_file_creations), str(num_shells),
+                                     str(num_access_files), str(num_outbound_cmds),
+                                     str(is_host_login), str(is_guest_login), str(count),
+                                     str(srv_count), str(serror_rate), str(srv_serror_rate),
+                                     str(rerror_rate), str(srv_rerror_rate), str(same_srv_rate),
+                                     str(diff_srv_rate), str(srv_diff_host_rate),
+                                     str(dst_host_count), str(dst_host_srv_count),
+                                     str(dst_host_same_srv_rate), str(dst_host_diff_srv_rate),
+                                     str(dst_host_same_src_port_rate),
+                                     str(dst_host_srv_diff_host_rate), str(dst_host_serror_rate),
+                                     str(dst_host_srv_serror_rate), str(dst_host_rerror_rate),
+                                     str(dst_host_srv_rerror_rate), str(attack_type)))
+
             # connection is not autocommit by default. So you must commit to save
             # your changes.
             self.connection.commit()
@@ -96,7 +120,7 @@ class SQLConnector(object):
         finally:
             pass
 
-    def read_gens(self, acc = 0):
+    def read_gens(self, acc=0):
         """ Reads from the gens table dependent on accuracy """
         try:
             with self.connection.cursor() as cursor:
@@ -107,8 +131,8 @@ class SQLConnector(object):
         finally:
             pass
 
-    #============================================= 
-    
+    #=============================================
+
     def read_specific_joined(self, theid, theiter):
         """ Reads the joined table at a specific id and iteration """
         try:
@@ -137,10 +161,9 @@ class SQLConnector(object):
         """ Writes a csv file output from a given dictionary """
 
         string = ""
-        for i in range(len(dictionaryarray)):  # just i in resiults?
-            dictionary = dictionaryarray[i]   # RESERVED!
+        for dictionary in dictionaryarray:  # just i in resiults?
             count = 0
-            for key, value in dictionary.items():
+            for _, value in dictionary.items():
                 string += str(value)
                 count = count + 1
                 if count < len(dictionary.items()):
@@ -158,19 +181,19 @@ class SQLConnector(object):
 def main():
     """ Auto run main method """
     conn = SQLConnector()
-    
+
     #conn.write_hyper(1, "2,3,4", "neptune", 40.3)
     #conn.write_gens(1, 1, 1, 0, "tcp", "ftp_data", "REJ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0.00, 171, 62, 0.27, 0.02, 0.01, 0.03, 0.01, 0, 0.29, 0.02, "portsweep")
-    
+
     #print(conn.read_gens())
     #print(conn.read_hyper())
-    
+
     #conn.sort_hyper()
 
     #print(conn.read_specific_joined(1, 1))
     #print(conn.read_joined(20))
     #conn.output_to_csv(conn.read_joined(20))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
 
