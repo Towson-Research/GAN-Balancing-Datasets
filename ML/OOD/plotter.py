6526acc17d20ea2cc7de51cf73914360bb3b5391
Jon Patton <jpatto1@students.towson.edu>
2019-05-09 12:36:47 -0400

    Update plotter.py

--- ML/OOD/plotter.py
+++ ML/OOD/plotter.py
@@ -30,8 +30,8 @@ Also, if you want to generate a bunch of figures during a loop without having to
 time, pass savefile = (something), mode = “save” and it’ll save the file. Just make sure you generate a unique name.
 '''
 
-def plot_3D(axes_data, X_label = "X", Y_label = "Y", Z_label = "Z", 
-            savefile = None, mode = "show", create_Z = True):
+def plot_3D(axes_data, x_label = "X", y_label = "Y", z_label = "Z", 
+            savefile = None, mode = "show", create_z = True, title = None):
     fig = plt.figure()
     if len(axes_data) == 1:
         # Make a 2D plot instead.
@@ -50,9 +50,10 @@ def plot_3D(axes_data, X_label = "X", Y_label = "Y", Z_label = "Z",
     ax.set_xlim(min(X), max(X))
     ax.set_ylim(min(Y), max(Y))
     ax.set_zlim(min(Z), max(Z))
-    ax.set_xlabel(X_label)
-    ax.set_ylabel(Y_label)
-    ax.set_zlabel(Z_label)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_zlabel(z_label)
+    plt.title(title)
 
     # Customize the view angle so it's easier to see that the scatter points lie
     # on the plane y=0
@@ -63,9 +64,10 @@ def plot_3D(axes_data, X_label = "X", Y_label = "Y", Z_label = "Z",
     else:
         plt.show()
 
-def plot_2D_multiline(axes_data, X_label = "X", Y_label = "Y", 
-            savefile = None, mode = "show", create_X = True):
+def plot_2D_multiline(axes_data, x_label = "X", y_label = "Y", 
+            savefile = None, mode = "show", create_x = True, title = None):
     fig = plt.figure()
+    ax = fig.add_subplot(111)
     Xs = []
     for data in axes_data:
         x = list(range(1, len(data)+ 1))
@@ -73,19 +75,80 @@ def plot_2D_multiline(axes_data, X_label = "X", Y_label = "Y",
     for X, Y in zip(Xs, axes_data):
         plt.plot(X, Y)
     
-    if mode.lower() == "save":
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    plt.title(title)
+    
+    if mode.lower() == "save" and savefile:
         plt.savefig(savefile)
     else:
         plt.show()
 
+def bar_graph(categories_values_lists, category_names = None, data_series_names = None, 
+                x_label = "X", y_label = "Y", mode = "show", title = None):
+    
+    n_groups = len(categories_values_lists[0])
+    n_bars = len(categories_values_lists)
+    if not data_series_names:
+        letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        data_series_names = []
+        for i in range(n_bars):
+            k = int(np.ceil((i + 1)/26))
+            data_series_names.append(letters[i % 26] * (k))
+    
+    if not category_names:
+        category_names = list(range(1, n_groups + 1))
+    
+    fig, ax = plt.subplots()
+    index = np.arange(n_groups)
+    bar_width = 0.2
+    opacity = 0.8
+    colors = ["b", "g", "r", "c", "m", "y", "k"]
+
+    print(category_names, data_series_names)
+
+    for i, series_name in enumerate(data_series_names):
+        p = bar_width * i + index
+        rect = plt.bar(p, categories_values_lists[i], bar_width,
+            alpha=opacity,
+            color = colors[i],
+            label = category_names[i])
+        autolabel(rect, ax)
+    
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+    plt.xticks(index + bar_width, category_names)
+    plt.title(title)
+    plt.legend()
+    plt.tight_layout()
+
+    if mode.lower() == "save" and savefile:
+        plt.savefig(savefile)
+    else:
+        plt.show()
+
+def autolabel(rects, ax):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.01*height,
+                '%d' % int(height),
+                ha='center', va='bottom', rotation = 90)
+
+
 def test():
-    n = 100
+    n = 10
     X = [random.randrange(25) for i in range(n)]
     Y = [random.randrange(25) for i in range(n)]
     Z = [random.randrange(25) for i in range(n)]
-    plot_3D([X, Y, Z], savefile = "test3d.png")
+    #plot_3D([X, Y, Z], savefile = "test3d.png")
 
-    plot_2D_multiline([X, Y, Z], savefile = "test3d.png")
+    #plot_2D_multiline([X, Y, Z], savefile = "test3d.png")
+
+    bar_graph([X, Y, Z])
 
 if __name__ == "__main__":
     test()
